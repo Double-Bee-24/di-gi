1fe39c29038313f2af527c97d7be50a02019eb63
Bohdan Bilokin <bodya24102001@gmail.com>
2023-12-27 06:27:28 +0200

    Added owlcarousel feature and adapted it to different dimensions, also favicon, title, and open-graph markup were added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            items: 1,
+            nav:true,
+            startPosition: 1,
+            navSpeed: 1600,
+            dots: true
+        }
+    );
 });
